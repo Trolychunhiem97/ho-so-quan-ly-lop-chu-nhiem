2906c8027d6dda5a80a33855b9bcb4aba23a0d7f
Trolychunhiem97 <transangabc2012@gmail.com>
2026-09-04 12:01:36 +0700

    Create danhsach.js
    Tạo dữ liệu danh sách học sinh lớp 97

---
+++ danhsach.js
@@ -0,0 +1,39 @@
+// DANH SÁCH HỌC SINH LỚP 97
+
+const danhSachHocSinh = [
+
+"Vũ Ngọc Ánh",
+"Trần Thị Ngọc Diệp",
+"Nguyễn Anh Duy",
+"Lý Gia Huy",
+"Hứa Bảo Khang",
+"Nguyễn Hoàng Khánh Linh",
+"Nguyễn Hoàng Nhật Linh",
+"Trương Đỗ Thục Linh",
+"Lê Quốc Long",
+"Nguyễn Bùi Hoàng Long",
+"Lâm Thị Trúc Ly",
+"Trần Lê Nhật Minh",
+"Vũ Thị Trà My",
+"Đỗ Phạm Hoàng Nam",
+"Bùi Thị Kim Ngân",
+"Hoàng Nguyễn Tuấn Ngọc",
+"Lô Thị Minh Nhi",
+"Nguyễn Thị Quỳnh Như",
+"Phạm Hoàng Thiên Phú",
+"Ngô Như Quỳnh",
+"Vũ Ngọc Như Quỳnh",
+"Trần Thị Thủy Tiên",
+"Hoàng Duy Thành",
+"Phạm Trường Thành",
+"Hoàng Thị Anh Thư",
+"Vũ Ngọc Anh Thư",
+"Trần Hoàng Bảo Trân",
+"Lô Thanh Trúc",
+"Nguyễn Thị Kiều Vi",
+"Nguyễn Thanh Vũ",
+"Hoàng Thị Thảo Vy",
+"Lâm Huỳnh Thảo Vy",
+"Nguyễn Ngô Hoàng Yến"
+
+];
